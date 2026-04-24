2d10d226bcdee2ab1fd3fa13c1cc59f286c964a1
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-24 23:27:35 +0530

    project

--- main.tf
+++ main.tf
@@ -3,3 +3,10 @@ resource "aws_security_group" "main" {
   description = var.sg_description
   vpc_id      = var.vpc_id
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
