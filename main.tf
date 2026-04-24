55c648206e3e62ce125945b1eab5d603070956b9
Narii Chowdary <nareshchowdary164@gmail.com>
2026-04-24 23:27:40 +0530

    project

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  tags = merge(
+    var.sg_tags,
+    local.common_tags,
+    {
+        Name = "${local.common_name_suffix}-${var.sg_name}" # roboshop-dev-catalogue
+    }
+  )
+}
